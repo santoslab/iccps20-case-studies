fc20e06e6c197464d588c298f934906333997fb4
Jason Belt <belt@ksu.edu>
2020-10-19 15:00:54 -0500

    example helper functions

--- building-control-mixed/hamr/src/c/ext-c/TempControl_i_tcp_tempControl/TempControl_i_tcp_tempControl.c
+++ building-control-mixed/hamr/src/c/ext-c/TempControl_i_tcp_tempControl/TempControl_i_tcp_tempControl.c
@@ -55,34 +55,48 @@ Unit building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl_handle
   setPoint = convertSetPointToFahrenheit(value);
 }
 
+void api_logInfo(STACK_FRAME String str) {
+  api_logInfo__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(CALLER str);
+}
+
+void api_send_fanCmd(STACK_FRAME building_control_mixed_BuildingControl_FanCmd_Type value){
+  api_send_fanCmd__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(CALLER value);
+}
+
+bool api_get_currentTemp(STACK_FRAME building_control_mixed_BuildingControl_Temperature_i currentTemp) {
+  return api_get_currentTemp__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(CALLER currentTemp);
+}
+
+building_control_mixed_BuildingControl_FanCmd_Type FanCmd_Type_On = building_control_mixed_BuildingControl_FanCmd_Type_On;
+building_control_mixed_BuildingControl_FanCmd_Type FanCmd_Type_Off = building_control_mixed_BuildingControl_FanCmd_Type_Off;
+
 Unit building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl_handletempChanged_(STACK_FRAME_ONLY) {
   DeclNewStackFrame(caller, "TempControl_i_tcp_tempControl.c", "", "building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl_handletempChanged_", 0);
 
   DeclNewbuilding_control_mixed_BuildingControl_Temperature_i(currentTemp);
-  if(api_get_currentTemp__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(SF &currentTemp)){
-    struct building_control_mixed_BuildingControl_Temperature_i currentTempInF = convertTemperatureToFahrenheit(&currentTemp);
+  if(api_get_currentTemp(SF &currentTemp)){
+    struct building_control_mixed_BuildingControl_Temperature_i currentTempInF = 
+      convertTemperatureToFahrenheit(&currentTemp);
     
     String str = string("received ");
     building_control_mixed_BuildingControl_Temperature_i_string_(SF str, &currentTemp);
-    api_logInfo__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(SF str);
+    api_logInfo(SF str);
     
     str = string("Sent fan command: ");
     if(currentTemp.degrees > setPoint.high.degrees) {
-      api_send_fanCmd__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(SF
-        building_control_mixed_BuildingControl_FanCmd_Type_On);
+      api_send_fanCmd(SF FanCmd_Type_On);
       building_control_mixed_BuildingControl_FanCmd_Type_string_(SF str, 
-        building_control_mixed_BuildingControl_FanCmd_Type_On);
+        FanCmd_Type_On);
     }
     else if(currentTemp.degrees < setPoint.low.degrees) {
-      api_send_fanCmd__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(SF
-        building_control_mixed_BuildingControl_FanCmd_Type_Off);
+      api_send_fanCmd(SF FanCmd_Type_Off);
       building_control_mixed_BuildingControl_FanCmd_Type_string_(SF str, 
-       building_control_mixed_BuildingControl_FanCmd_Type_Off);
+       FanCmd_Type_Off);
     } else {
       str = string("Temperature ok:");
     }
     
-    api_logInfo__building_control_mixed_BuildingControl_TempControl_i_tcp_tempControl(SF str);
+    api_logInfo(SF str);
   } 
 }
 
